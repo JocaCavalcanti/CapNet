77af5c30e8e493875525d0b2ecb54ed0a86b4965
JocaCavalcanti <joaomcavalcanti.jmcm@gmail.com>
2024-09-30 17:13:18 -0300

    commit add: add new images and add new button

--- app/index.tsx
+++ app/index.tsx
@@ -1,18 +1,33 @@
-import { View, Text, Button } from 'react-native';
+import { View,  Image } from 'react-native';
 import React from 'react';
 import { useRouter } from 'expo-router';
+import { Button } from 'react-native-paper';
 
 export default function Index() {
   const router = useRouter();
 
   const handleNavigate = () => {
-    router.push("/(tabs)/home"); 
+    router.push("/(tabs)/home");
   };
 
   return (
-    <View>
-      <Text>Index</Text>
-      <Button title="Ir para Tabs" onPress={handleNavigate} />
+    <View
+      style={{
+        flex: 1,
+        justifyContent: 'center',
+        alignItems: 'center',
+      }}
+    >
+      <Image
+        source={require('../assets/images/logoScreen.png')}
+        style={{
+          width: '100%',
+          height: 300,
+        }}
+      />
+      <Button mode="contained" onPress={handleNavigate}>
+    Press me
+  </Button>
     </View>
   );
 }
